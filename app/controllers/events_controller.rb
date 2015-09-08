f9283920bcb7a951ad398c6a568aac4eb12e3e87
hoshikyu <hoshikyu@sfc.wide.ad.jp>
2015-09-08 14:07:45 +0900

    create newEvents

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,27 @@
 class EventsController < ApplicationController
+  def new
+  	@event = Event.new
+  end
+
   def create
+	 @event = Event.new(event_params)
+	  if @event.save
+	    # @userはuser_path(@user) に自動変換される
+	    redirect_to :action => "list"
+	  else
+	    # ValidationエラーなどでDBに保存できない場合 new.html.erb を再表示
+	    render 'new'
+	  end
   end
 
   def list
   end
+
+  private
+  # Rails4からStrongParamaterと呼ばれる機能が追加されました。
+  # セキュリティのため、permitメソッドで許可したパラメータ名しか取得できません。
+  def event_params
+    params.require(:event).permit(:title, :owner, :date ,:deadline ,:detail)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'events/create'
+
+  resources :events
 
   get 'events/list'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
