32c1ad31974be311dde153b6be0c31ce154f6f8b
sttr110 <mydogs@live.jp>
2015-09-08 14:49:20 +0900

    参加者migrationファイル作成 参加者モデル作成

---
+++ db/migrate/20150908032327_create_participants.rb
@@ -0,0 +1,12 @@
+class CreateParticipants < ActiveRecord::Migration
+  def change
+    create_table :participants do |t|
+      t.integer :event_id
+      t.integer :user_id
+      t.string :user_name
+      t.column :status, :enum, :limit => [:participanted, :cancelled, :unanswered], :default => :unanswered
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150908020303) do
+ActiveRecord::Schema.define(version: 20150908032327) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -22,5 +22,8 @@ ActiveRecord::Schema.define(version: 20150908020303) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+# Could not dump table "participants" because of following NoMethodError
+#   undefined method `[]' for nil:NilClass
 
 end
