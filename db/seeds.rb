a0144a9c13941e6145dd7553d3684296425dae10
sttr110 <mydogs@live.jp>
2015-09-09 11:12:49 +0900

    Userモデルとseedの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,35 +7,37 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Event.create(
-  :title    => 'event1',
-  :owner    => 'owner1',
-  :date     => '2015-10-25 00:00:00',
-  :deadline => '2015-10-30 00:00:00',
-  :detail   => 'text text text text'
+  :title     => 'event1',
+  :owner     => 'owner1',
+  :date      => '2015-10-25 00:00:00',
+  :deadline  => '2015-10-30 00:00:00',
+  :detail    => 'text text text text'
 )
 Event.create(
-  :title    => 'event2',
-  :owner    => 'owner2',
-  :date     => '2015-11-25 00:00:00',
-  :deadline => '2015-11-30 00:00:00',
-  :detail   => 'text text text text'
+  :title     => 'event2',
+  :owner     => 'owner2',
+  :date      => '2015-11-25 00:00:00',
+  :deadline  => '2015-11-30 00:00:00',
+  :detail    => 'text text text text'
 )
 Event.create(
-  :title    => 'event3',
-  :owner    => 'owner3',
-  :date     => '2015-12-25 00:00:00',
-  :deadline => '2015-12-30 00:00:00',
-  :detail   => 'text text text text'
+  :title     => 'event3',
+  :owner     => 'owner3',
+  :date      => '2015-12-25 00:00:00',
+  :deadline  => '2015-12-30 00:00:00',
+  :detail    => 'text text text text'
 )
 Participant.create(
-  :event_id => 1,
+  :event_id  => 1,
   :user_id   => 1,
   :user_name => 'userName1',
 )
 Participant.create(
-  :event_id => 1,
+  :event_id  => 1,
   :user_id   => 2,
   :user_name => 'userName2',
-  :status => :ancelled,
+  :status    => :ancelled,
 )
-
+User.create(
+  :name      => 'TestUser'
+)
