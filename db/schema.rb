15405c059f7553871af20966637ee324eed7be10
HayashiTomoki <b15725bb@aitech.ac.jp>
2015-09-09 18:12:52 +0900

    Revert "イベント参加・キャンセル機能追加　userモデルも作成"

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150909020845) do
+ActiveRecord::Schema.define(version: 20150908032327) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -25,11 +25,5 @@ ActiveRecord::Schema.define(version: 20150909020845) do
 
 # Could not dump table "participants" because of following NoMethodError
 #   undefined method `[]' for nil:NilClass
-
-  create_table "users", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
 
 end
