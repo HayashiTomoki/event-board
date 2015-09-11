29478e4976488effb11072420e8d748186070b8e
HayashiTomoki <b15725bb@aitech.ac.jp>
2015-09-11 11:23:25 +0900

    参加者テーブルのバリデーション追加

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,4 +1,15 @@
 class Participant < ActiveRecord::Base
   belongs_to :event
   belongs_to :user
+
+  validate :deadline_past, on: :update
+
+
+#参加期限が過ぎていた時のバリデーション
+  def deadline_past
+    event = Event.find(event_id)
+    if event.deadline < Time.now.to_s(:db)
+      errors.add(:deadline_post, "回答期限をすぎています")
+    end
+  end
 end
